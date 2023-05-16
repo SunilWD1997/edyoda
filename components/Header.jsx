4fa11a4fdf2edc7cc1017fb877338a3e8e899549
Sunil <npatra412@gmail.com>
2023-05-16 23:23:01 +0530

    second_commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,20 +13,20 @@ const Header = () => {
         <div className="flex  gap-12">
           {/*Site logo starts here */}
           <Link href="/">
-            <Image src="/logo.png" width="150" height="100" alt="logo" />
+            <Image src="/logo.png" width="150" height="100" alt="logo" className="hover:scale-[105%] duration-100"/>
           </Link>
           {/*Site logo ends here */}
 
           {/* Courses menu starts here */}
           <Link href="/" className="flex items-center gap-1 font-medium ">
-            <span className="text-[16px] ">Courses</span>
+            <span className="text-[16px] hover:text-[grey]">Courses</span>
             <MdKeyboardArrowDown className="text-xl" />
           </Link>
           {/*Courses menu ends here */}
 
           {/* Courses menu starts here */}
           <Link href="/" className="flex items-center gap-1 font-medium ">
-            <span className="text-[16px] ">Programs</span>
+            <span className="text-[16px] hover:text-[grey]">Programs</span>
             <MdKeyboardArrowDown className="text-xl" />
           </Link>
           {/*Courses menu ends here */}
@@ -36,15 +36,15 @@ const Header = () => {
         {/* right columns starts here */}
         <div className="flex items-center gap-12">
           {/* search logo starts here */}
-          <AiOutlineSearch className="text-[25px]" />
+          <AiOutlineSearch className="text-[25px] hover:text-[grey] cursor-pointer" />
           {/* search logo starts here */}
 
           {/* LogIn starts here */}
-          <Link href="/" className="font-bold">Log in</Link>
+          <Link href="/" className="font-bold hover:text-[grey]">Log in</Link>
           {/* LogIn ends here */}
 
           {/* Join now button starts here */}
-          <button className="text-[white] px-10 py-2 rounded-[50px] font-bold bg-gradient-to-r from-[#0048FF] to-[#0096FF]">
+          <button className="text-[white] px-10 py-2 rounded-[50px] font-bold  bg-gradient-to-r from-[#0048FF] to-[#0096FF] hover:scale-[97%] duration-100 ">
             <Link href="/">JOIN NOW</Link>
           </button>
           {/* Join now button  ends here */}
--- components/Subscription.jsx
+++ components/Subscription.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useContext } from "react";
 import { rateContext } from "@/pages";
@@ -7,27 +7,28 @@ import {RiTimerLine} from 'react-icons/ri';
 
 const Subscription = () => {
 const{setRate, rate} = useContext(rateContext)
+
+const[subs, setSubs]=useState(true)
     
   return (
     <div className="w-[100%] flex flex-col  gap-3">
 
 
    {/* 12 months expired subscription starts here */}
-   <label className="w-[100%] cursor-pointer border-[3px]  rounded-[4px] relative" onClick={()=>{setRate(alert('this offer is expired')), setRate(179)}}>
+   <label className="w-[100%] cursor-pointer border-[3px]  rounded-[4px] relative" onClick={()=>{setRate(alert('Sorry, this offer is expired')), setRate(179)}}>
         <input type="radio" className=" peer sr-only" name="pricing" />
 
-        <div className=" max-w-xl rounded-md bg-white py-1 px-3  text-gray-600 ring-2 ring-transparent transition-all   ">
+        <div className=" max-w-xl rounded-md bg-[#f8f8f8] py-1 px-3  text-gray-200 ring-2 ring-transparent transition-all   ">
           <div className="flex flex-col gap-1">
             <div className="flex items-center justify-between">
               <div className="flex gap-4">
-                <svg width="24" height="24" viewBox="0 0 24 24">
-                  <path
-                    fill="currentColor"
-                    d="m10.6 13.8l-2.175-2.175q-.275-.275-.675-.275t-.7.3q-.275.275-.275.7q0 .425.275.7L9.9 15.9q.275.275.7.275q.425 0 .7-.275l5.675-5.675q.275-.275.275-.675t-.3-.7q-.275-.275-.7-.275q-.425 0-.7.275ZM12 22q-2.075 0-3.9-.788q-1.825-.787-3.175-2.137q-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175q1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138q1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175q-1.35 1.35-3.175 2.137Q14.075 22 12 22Z"
-                  />
-                </svg>
 
-                <p className="text-[#3C4852] font-Roboto font-[600]">
+              <div className="text-[#BEBEBE] w-[24px] h-[24px] border-[3px] rounded-full flex justify-center items-center">
+              <div className="text-[#BEBEBE] w-[13px] h-[13px] bg-[#e4e1e1] rounded-[42%] "></div>
+
+              </div>
+
+                <p className="text-[#94a3b1] font-Roboto font-[600]">
                   12 Months Subscription
                 </p>
               </div>
@@ -142,9 +143,10 @@ const{setRate, rate} = useContext(rateContext)
                     d="m10.6 13.8l-2.175-2.175q-.275-.275-.675-.275t-.7.3q-.275.275-.275.7q0 .425.275.7L9.9 15.9q.275.275.7.275q.425 0 .7-.275l5.675-5.675q.275-.275.275-.675t-.3-.7q-.275-.275-.7-.275q-.425 0-.7.275ZM12 22q-2.075 0-3.9-.788q-1.825-.787-3.175-2.137q-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175q1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138q1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175q-1.35 1.35-3.175 2.137Q14.075 22 12 22Z"
                   />
                 </svg>
+               
 
                 <p className="text-[#3C4852] font-Roboto font-[600]">
-                  6 Months Subscription
+                  3 Months Subscription
                 </p>
               </div>
 
@@ -162,6 +164,7 @@ const{setRate, rate} = useContext(rateContext)
       </label>
 
 {/* 3 months subscription ends here */}
+
    
 
   <div className="h-[2px] bg-[#BEBEBE] mt-1"></div>
